5926537c738108e7a1edad95560f7d97fe244e1d
Matthew Nilsen <mnilsen50@gmail.com>
2019-02-20 10:33:40 -0500

    Using SkillsData in SkillSelectionViewModel instead of Map

--- app/src/main/java/edu/gatech/cs2340/spacetrader/entity/SkillsData.kt
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/entity/SkillsData.kt
@@ -1,7 +1,27 @@
 package edu.gatech.cs2340.spacetrader.entity
 
-data class SkillsData(var pilot: Int, var fighter: Int, var trader: Int, var engineer: Int) {
+data class SkillsData(var skillsMap: MutableMap<Skill, Int>) {
+
+    init{
+        for(s: Skill in Skill.values()) {
+            skillsMap[s] = 0
+        }
+    }
+
+    fun addPoint(s: Skill) {
+        skillsMap[s] = skillsMap[s]!! + 1
+    }
+
+    fun removePoint(s: Skill) {
+        skillsMap[s] = skillsMap[s]!! - 1
+    }
+
     override fun toString(): String {
-        return "pilot: {$pilot}, fighter: {$fighter}, trader: {$trader}, and engineer: {$engineer}"
+        var s: String = ""
+        skillsMap.forEach{
+            (skill, value) -> s += "$skill: $value, "
+        }
+        s = s.substring(0, s.length - 2)
+        return s
     }
 }
--- app/src/main/java/edu/gatech/cs2340/spacetrader/viewmodel/SkillSelectionViewModel.kt
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/viewmodel/SkillSelectionViewModel.kt
@@ -6,28 +6,22 @@ import edu.gatech.cs2340.spacetrader.entity.SkillsData
 
 class SkillSelectionViewModel(): GameSetup<SkillsData> {
     private var skillMap = mutableMapOf<Skill, Int>()
-    private val skills = Skill.values()
+    private val skills = SkillsData(skillMap)
     private val maxSkillPoints = 16
     private var usedSkillPoints = 0
 
-    init {
-        for(s : Skill in skills) {
-            skillMap[s] = 0
-        }
-    }
-
     fun addSkillPoint(skill : Skill) {
-        Log.d("SSVM", "Adding: ${usedSkillPoints} + 1 = ${usedSkillPoints + 1}")
+        //Log.d("SSVM", "Adding: ${usedSkillPoints} + 1 = ${usedSkillPoints + 1}")
         if (usedSkillPoints < maxSkillPoints) {
-            skillMap[skill] = skillMap[skill]!! + 1
+            skills.addPoint(skill)
             usedSkillPoints++
         }
     }
 
     fun removeSkillPoint(skill : Skill) {
-        Log.d("SSVM", "Removing: ${usedSkillPoints} - 1 = ${usedSkillPoints - 1}")
+        //Log.d("SSVM", "Removing: ${usedSkillPoints} - 1 = ${usedSkillPoints - 1}")
         if (usedSkillPoints > 0 && skillMap[skill]!! > 0) {
-            skillMap[skill] = skillMap[skill]!! - 1
+            skills.removePoint(skill)
             usedSkillPoints--
         }
     }
@@ -51,6 +45,6 @@ class SkillSelectionViewModel(): GameSetup<SkillsData> {
     }
 
     override fun passPlayerData(): SkillsData {
-        return SkillsData(skillMap[Skill.PILOT]!!, skillMap[Skill.FIGHTER]!!, skillMap[Skill.TRADER]!!, skillMap[Skill.ENGINEER]!!)
+        return skills
     }
 }
