6c5880dae7131b3b3b04f1d89ce4685a5d1e06fd
lordbobby <zachmcgee99@gmail.com>
2019-02-22 16:46:25 -0500

    more structure for M6

---
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/model/GameManager.kt
@@ -0,0 +1,7 @@
+package edu.gatech.cs2340.spacetrader.model
+
+class GameManager(private val player: Player, val difficulty: GameDifficulty) {
+
+    private val universe: Universe = Universe()
+
+}
