c21f1d211bf6d24965ce8cbdfbfcfa702f3ac73a
Angelo <angelozheng2000@gmail.com>
2019-02-22 18:55:46 -0500

    Implemented CoordinateGenerator

--- app/src/main/java/edu/gatech/cs2340/spacetrader/generators/PlanetGenerator.kt
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/generators/PlanetGenerator.kt
@@ -3,18 +3,18 @@ package edu.gatech.cs2340.spacetrader.generators
 import edu.gatech.cs2340.spacetrader.entity.PlanetResource
 import edu.gatech.cs2340.spacetrader.entity.TechLevel
 import edu.gatech.cs2340.spacetrader.model.Planet
-import edu.gatech.cs2340.spacetrader.model.SolarSystem
 import edu.gatech.cs2340.spacetrader.util.Coordinate
 
-class PlanetGenerator(var planets:Int, val names: NameProvider): Generator<Planet> {
+class PlanetGenerator(private var planets:Int, private val names: NameProvider, private val coordinateGen: CoordinateGen): MappedGenerator<Planet> {
     private val resourceTypes = PlanetResource.values()
     private val techLevels = TechLevel.values()
 
     override fun generate(): Map<Coordinate, Planet> {
-        var solarSystem = HashMap<Coordinate, Planet>()
+        val solarSystem = HashMap<Coordinate, Planet>()
+        coordinateGen.generateBasePoint()
 
         while(planets > 0) {
-            val coord = Coordinate()
+            val coord = coordinateGen.generate()
             val resource = resourceTypes[(0 until resourceTypes.size - 1).random()]
             val techLevel = techLevels[(0 until techLevels.size - 1).random()]
             val name = names.next()
