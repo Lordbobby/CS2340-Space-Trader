d7d371b114e45214b5afe70af7782235d2cf2c6b
Matthew Nilsen <mnilsen50@gmail.com>
2019-04-07 14:54:16 -0400

    Spelling fixes

--- app/src/test/java/edu/gatech/cs2340/spacetrader/CoordinateEqualsTest.java
+++ app/src/test/java/edu/gatech/cs2340/spacetrader/CoordinateEqualsTest.java
@@ -11,7 +11,11 @@ import static org.junit.Assert.assertEquals;
  * Test for Coordinate equals method
  */
 public class CoordinateEqualsTest {
+
     @Test
+    /**
+     * Test for equals method
+     */
     public void equals() {
         Coordinate c1 = new Coordinate(0, 0);
         Object fakeCoord = new Object();
